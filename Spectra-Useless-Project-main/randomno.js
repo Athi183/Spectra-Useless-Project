b1365eb31dc672a437616cbca92264e0bfc42f3a
dreamyshard18 <diyalesu@gmail.com>
2024-11-04 00:05:39 +0530

    do

--- Spectra-Useless-Project-main/randomno.js
+++ Spectra-Useless-Project-main/randomno.js
@@ -1,5 +1,16 @@
 function randNum(){
     cpercent = document.getElementById("cpercent").value;
     result=document.getElementById("result");
-    result.innerHTML=Math.floor(Math.random()*(cpercent*5))-0;
+    if(cpercent<50){
+    result.innerHTML=Math.floor(Math.random()*(cpercent*1))-0;
+    }
+    else if(cpercent>50 && cpercent<=200){
+        result.innerHTML=Math.floor(Math.random()*(cpercent*0.5))-0;
+    }
+    else if(cpercent>200 && cpercent<=1000){
+        result.innerHTML=Math.floor(Math.random()*(cpercent*0.15))-0;
+    }
+    else{
+        result.innerHTML=Math.floor(Math.random()*(cpercent*0.005))-0;  
+    }
 }
