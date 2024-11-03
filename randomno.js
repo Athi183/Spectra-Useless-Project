70d9fdcdaf755e76a0a4507c65ac21816210ae98
megzzz05 <meghnaveliath@gmail.com>
2024-11-03 12:07:29 +0530

    random

---
+++ randomno.js
@@ -0,0 +1,5 @@
+function randNum(){
+    cpercent = document.getElementById("cpercent");
+    result=document.getElementById("result");
+    result.innerHTML=Math.floor(Math.random()*cpercent)-0;
+}
